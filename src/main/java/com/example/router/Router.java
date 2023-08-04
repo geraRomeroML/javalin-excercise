a8d6e35715f6fc772c95c8b5a4ffc5cae6fc23ed
erromero <gerardo.eromero@mercadolibre.com>
2023-08-04 10:44:33 -0300

    added endpoints to retrieve only one or multiple items

--- src/main/java/com/example/router/Router.java
+++ src/main/java/com/example/router/Router.java
@@ -1,22 +1,33 @@
 package com.example.router;
 
 import com.example.DependencyInjectionModule;
+import com.example.controllers.ItemController;
 import com.google.common.base.Stopwatch;
 import com.google.inject.Injector;
 import io.javalin.apibuilder.EndpointGroup;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.Optional;
 import java.util.concurrent.TimeUnit;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
 
 @Slf4j
 public class Router implements EndpointGroup {
+    private ItemController itemController;
+
     @Override
     public void addEndpoints() {
         Injector injector = DependencyInjectionModule.getInstance();
         Stopwatch stopwatch = Stopwatch.createStarted();
 
+        itemController = injector.getInstance(ItemController.class);
+
+        get("/items/{itemId}", itemController::getItem);
+        get("/advertising/product_ads/ads", itemController::getItems);
+
         long timeTakenToExecuteRouterInit = stopwatch.elapsed(TimeUnit.SECONDS);
-        log.info("[init] Time taken to execute Router.init method: {}s", timeTakenToExecuteRouterInit);
+        log.info("[init] Time taken to execute Router.init method: {}s", Optional.of(timeTakenToExecuteRouterInit));
 
     }
 }
